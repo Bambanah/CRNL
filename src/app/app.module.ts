fc4023f2e3f727cd10d8bf4e53dd6603cff9cb68
Lachlan Underhill <lachlanu@gmail.com>
2018-09-24 01:52:56 +1000

    Create simple home and student pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { HeaderComponent } from './shared/layout/header.component';
-import { FooterComponent } from './shared/layout/footer.component';
 
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { AppRoutingModule } from './app-routing.module';
+import { StudentsComponent } from './students/students.component';
+import { StudentDetailComponent } from './student-detail/student-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    StudentsComponent,
+    StudentDetailComponent
   ],
   imports: [
     BrowserModule,
     BsDropdownModule.forRoot(),
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
