c824b3e0a1a7a8ef2ca56ac98c452fa68d8dbd03
Lachlan Underhill <lachlanu@gmail.com>
2018-10-29 13:02:02 +1000

    User list returns list of all users

--- src/app/users/user-list/user-list.component.ts
+++ src/app/users/user-list/user-list.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
+import {ApiService} from '../../_services/api.service';
+import { Observable } from 'rxjs';
+import {User} from '../../_models/User.js';
+
+export interface UserData {
+  name: string;
+  major: string;
+  minor: string;
+  email: string;
+}
 
 @Component({
   selector: 'app-user-list',
@@ -6,8 +17,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-list.component.scss']
 })
 export class UserListComponent implements OnInit {
+  displayedColumns: string[] = ['name', 'major', 'minor', 'email'];
+  users: Observable<User>;
 
-  constructor() { }
+  constructor(private api: ApiService) {
+    this.users = api.getUsers();
+  }
 
   ngOnInit() {
   }
