b4ef08a7dd233c5b50ef282342ab7e4a1b51a3e3
Unknown <lachlanu@gmail.com>
2019-02-07 19:07:20 +0700

    Updated test imports

--- src/app/auth/signup-modal/signup-modal.component.spec.ts
+++ src/app/auth/signup-modal/signup-modal.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SignupModalComponent } from './signup-modal.component';
+import { SignupComponent } from '../signup/signup.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('SignupModalComponent', () => {
   let component: SignupModalComponent;
@@ -8,7 +12,12 @@ describe('SignupModalComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SignupModalComponent ]
+      declarations: [ SignupModalComponent, SignupComponent ],
+      imports: [
+        ReactiveFormsModule,
+        RouterTestingModule,
+        HttpClientModule
+      ]
     })
     .compileComponents();
   }));
--- src/app/shared/main-nav/main-nav.component.spec.ts
+++ src/app/shared/main-nav/main-nav.component.spec.ts
@@ -6,6 +6,11 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { MatToolbarModule } from '@angular/material';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClientModule } from '@angular/common/http';
+import { LoginModalComponent } from 'src/app/auth/login-modal/login-modal.component';
+import { SignupModalComponent } from 'src/app/auth/signup-modal/signup-modal.component';
+import { LoginComponent } from 'src/app/auth/login/login.component';
+import { SignupComponent } from 'src/app/auth/signup/signup.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('MainNavComponent', () => {
   let component: MainNavComponent;
@@ -13,12 +18,13 @@ describe('MainNavComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [MainNavComponent],
+      declarations: [MainNavComponent, LoginModalComponent, SignupModalComponent, LoginComponent, SignupComponent],
       imports: [
         MatToolbarModule,
         RouterTestingModule,
         NgbModule,
-        HttpClientModule
+        HttpClientModule,
+        ReactiveFormsModule
       ]
     }).compileComponents();
   }));
