cc779c5bad261667678dfaae11980f516a1b9788
Lachlan Underhill <lachlanu@gmail.com>
2019-03-04 15:05:41 +1000

    Reinitialised student model

--- src/app/_models/Student.js
+++ src/app/_models/Student.js
@@ -1,21 +1,21 @@
-// const User = require('./User');
-// const mongoose = require('mongoose');
-// const Schema = mongoose.Schema;
+const User = require('./User');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-// const Student = User.discriminator(
-//   'Student',
-//   new Schema({
-//     major: {
-//       type: String,
-//       enum: ['Computer Science', 'Information Systems', 'Unspecified'],
-//       default: 'Unspecified'
-//     },
-//     minor: {
-//       type: String,
-//       enum: ['Intelligent Systems', 'User Experience', 'Unspecified'],
-//       default: 'Unspecified'
-//     }
-//   })
-// );
+const Student = User.discriminator(
+  'Student',
+  new Schema({
+    major: {
+      type: String,
+      enum: ['Computer Science', 'Information Systems', 'Unspecified'],
+      default: 'Unspecified'
+    },
+    minor: {
+      type: String,
+      enum: ['Intelligent Systems', 'User Experience', 'Unspecified'],
+      default: 'Unspecified'
+    }
+  })
+);
 
-// module.exports = mongoose.model('Student');
+module.exports = mongoose.model('Student');
