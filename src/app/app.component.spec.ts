67670aeb78896c8cfdeb20e39e78e396b2c78f57
Lachlan Underhill <lachlanu@gmail.com>
2019-03-07 16:18:13 +1000

    Updated Tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -38,9 +38,4 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'QUT Compass'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('QUT Compass');
-  }));
 });
