4d506b109030470f86e8c1c67eb4e5d26767e49f
Lachlan Underhill <lachlanu@gmail.com>
2019-03-22 01:52:34 +1000

    Deprecated role.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AuthGuard } from './_guards/auth.guard';
-import { Role } from './_models/role';
 
 import { HomeComponent } from './home/home.component';
 import { UserListComponent } from './users/user-list/user-list.component';
