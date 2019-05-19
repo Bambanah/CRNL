6796eb56859811999969b4c570d7356ed43c4c60
Lachlan Underhill <lachlanu@gmail.com>
2019-05-19 10:24:45 +1000

    Formatting

--- src/app/_routes/api-routes.js
+++ src/app/_routes/api-routes.js
@@ -47,13 +47,12 @@ router.get('/users/:id', function(req, res, next) {
 
 // Update single user
 router.put('/users/:id', function(req, res, next) {
-  Student.findByIdAndUpdate(
-    req.params.id, 
-    req.body,
-    { new: true}, 
-    function(err, Student) {
-      if (err) return next(err);
-      res.json(Student);
+  Student.findByIdAndUpdate(req.params.id, req.body, { new: true }, function(
+    err,
+    Student
+  ) {
+    if (err) return next(err);
+    res.json(Student);
   });
 });
 
